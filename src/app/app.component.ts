2672f1173f89b50557cc918743ac993e6523ea58
ale <tiffanyalejandrina@gmail.com>
2025-01-18 00:23:36 -0600

    Revert "se crean funcionalidades de lo solicitado (faltante pago)"
    This reverts commit 7b75550a4960fb87e721302246857d24adddae18.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'prueba-tecnica';
- constructor(private router: Router) {}
-
-
- ngOnInit() {
-  const token = localStorage.getItem('JwtToken');
-    if (token) {
-      // Si hay un token, redirigir a otro componente (por ejemplo, el dashboard)
-      this.router.navigate(['menu']);
-    } else {
-      // Si no hay un token, redirigir al login
-      this.router.navigate(['login']);
-    }
-  }
 }
-
-
